989c647e174c126a4bcd221f5b5816ab0fb2b985
Babiyv <377064910Vo>
2021-07-08 03:04:09 +0300

    initial commit

---
+++ src/main/java/lesson13/homework/Main.java
@@ -0,0 +1,10 @@
+package lesson13.homework;
+
+/*1.Создать проект Maven с базовыми конфигурациями на свое усмотрение,
+добавить пару зависимостей(например lombok и на свое усмотрение),
+прописать версию проекта не ниже java 8 и залить на GIT;*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First maven project is working");
+    }
+}
